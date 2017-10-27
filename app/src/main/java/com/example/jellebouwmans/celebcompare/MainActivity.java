eb1d2bb3555fad4fc53aa5018694da7ba45e73a7
Bouwmans,Jelle J.P.A <jpa.bouwmans@student.fontys.nl>
2017-10-27 14:29:57 +0200

    LoginGUI

--- app/src/main/java/com/example/jellebouwmans/celebcompare/MainActivity.java
+++ app/src/main/java/com/example/jellebouwmans/celebcompare/MainActivity.java
@@ -1,17 +1,18 @@
 package com.example.jellebouwmans.celebcompare;
 
+import android.content.res.AssetManager;
 import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.TextView;
 
-public class LoginActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_main);
 
         Fonts();
     }
@@ -19,16 +20,5 @@ public class LoginActivity extends AppCompatActivity {
     private void Fonts(){
         //FONT
         Typeface custom_font = Typeface.createFromAsset(getAssets(),  "fonts/topmodern.ttf");
-
-        //Textfield Username
-        TextView tx = (TextView)findViewById(R.id.txtPassword);
-        tx.setTypeface(custom_font);
-
-        //Textfield Password
-        TextView pass = (TextView)findViewById(R.id.txtUsername);
-        pass.setTypeface(custom_font);
-
-        Button btnLogin = (Button)findViewById(R.id.btnLogin);
-        btnLogin.setTypeface(custom_font);
     }
 }
